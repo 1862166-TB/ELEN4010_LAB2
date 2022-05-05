39f40934b13f4f7aa833c18437df1f04c58e9352
1862166-TB <1862166@students.wits.ac.za>
2022-05-05 12:34:21 +0200

    All code

---
+++ index.js
@@ -0,0 +1,34 @@
+'use strict'
+const path = require('path') // used later in the exercise
+const express = require('express')
+const app = express()
+/*
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+app.get('/about', function (req, res) {
+  res.sendFile(path.join(__dirname, 'views', 'about.html'))
+})
+
+app.listen(3000)
+console.log('Express server running on port 3000')
+*/
+// keep the first 3 lines from before
+const mainRouter = require('./mainRoutes')
+/*
+mainRouter.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+mainRouter.get('/about', function (req, res) {
+  res.sendFile(path.join(__dirname, 'views', 'about.html'))
+})
+*/
+app.use(mainRouter)
+
+/*app.listen(3000)
+console.log('Express server running on port 3000')*/
+const port = process.env.PORT || 3000
+app.listen(port)
+console.log('Express server running on port', port)
+
